689cc333d57dff44155ff0b29e94ecc281adeaf4
Diego Moreno <diego.morenodam25@gmail.com>
2024-06-11 17:08:01 -0600

    Commit inicial de la calculadora

---
+++ Calculadora.c
@@ -0,0 +1,46 @@
+# include <stdio.h>
+
+int main(){
+
+    int opcion, num1, num2;
+    while(1){
+    printf("1-Suman\n2-Resta\n3-Multiplicacion\n4-Divicion\n5-salir\n");
+    printf("Selecciona el numero de la funcion que quieres hacer (1-4): ");
+    scanf("%d", &opcion);
+
+    if(opcion > 5 || opcion < 1){
+        printf("Error. Selecciona un numero que si este dentro del rango (1-5)\n");
+    }
+    if(opcion == 5){
+                    printf("Saliendo del programa...");
+            break;
+        }
+
+    switch(opcion){
+        //suma
+        case 1: printf("Dijita el primer dijito: ");
+        break;
+        //resta
+        case 2: printf("Dijita el primer dijito: ");
+        break;
+        //multiplicacion 
+        case 3: printf("Dijita el primer dijito: ");
+        break;
+        //Divicion
+        case 4: printf("Dijita el primer dijito: ");
+        break;
+        //salir     
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+    return 0;
+}
